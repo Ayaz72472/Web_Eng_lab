b8e6b6fdd1e47acc1e7ef4a35db34f89ffc3d3db
Ayaz Hussian <99063481+Ayaz72472@users.noreply.github.com>
2024-05-23 21:27:40 +0500

    Add files via upload

--- Class_practice/practice.js
+++ Class_practice/practice.js
@@ -1,16 +1,2 @@
-/*var h1 = document.getElementById("header1")
-h1.style.color = "red";
-
-function alertMessage() {
-        alert("Button Pressed");
-    }
-*/
-function createBall() {
-    var ball = document.createElement("div");
-
-    ball.classList.add("ball");
-
-    document.querySelector(".container").appendChild(ball);
-}
-
-window.onload = createBall;
+const arr = [1,2,3,4,5,6,7]
+arr.map
